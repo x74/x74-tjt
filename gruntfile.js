8ac02813716ac35c0d4b49fb2dd5e296a6acd8b2
x74 <x74@localhost>
2016-10-26 23:06:34 +0200

    adding htmlmin

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,17 @@
 module.exports = function(grunt) {
+    // Show elapsed time after tasks run
+    require('time-grunt')(grunt);
+    // Load all Grunt tasks
+    //require('jit-grunt')(grunt);
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
+        app: {
+            source: '_site',
+            dist: '_site_dist',
+            baseurl: ''
+		},
         uglify: {
             build: {
                 src: 'js/*.js',
@@ -13,7 +21,8 @@ module.exports = function(grunt) {
 
         sass: {
             options: {
-                style: 'expanded',
+                //style: 'expanded', // compressed, compact, expanded, nested
+                outputStyle: 'compact',
                 sourceMap: true
             },
             dist: {
@@ -74,13 +83,43 @@ module.exports = function(grunt) {
             jekyllBuild: {
                 command: "jekyll build"
             }
-        }
+        },
+        
+         htmlmin: {                                     // Task
+				dist: {                                      // Target
+				  options: {                                 // Target options
+					removeComments: true,
+					collapseWhitespace: true,
+					collapseBooleanAttributes: true,
+                    removeAttributeQuotes: true,
+                    removeRedundantAttributes: true,
+                    removeEmptyAttributes: true,
+                    minifyJS: true,
+					minifyCSS: true
+					},
+				  files: [{                                   // Dictionary of files
+					expand: true,
+					cwd: '_site',
+					src: '**/*.html',
+					dest: '_site_min'
+//					'_site/index3.html': '_site/index.html'     // 'destination': 'source'
+					}]
+				}
+//				dev: {                                       // Another target
+//				  files: {
+//					'_site_dist/index.html': 'src/index.html',
+//					'_site_dist/contact.html': 'src/contact.html'
+//				  }
+//				}
+		  }
     });
 
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     //grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-sass');
+    //grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-shell');
@@ -89,10 +128,13 @@ module.exports = function(grunt) {
     // Default task(s).
 
     grunt.registerTask("serve", ["shell:jekyllServe"]);
+//    grunt.registerTask("build", ["sass", "autoprefixer", "uglify", "shell:jekyllBuild"]);
+    grunt.registerTask("build", ["sass", "autoprefixer", "shell:jekyllBuild"]);
     grunt.registerTask("default", ["sass", "autoprefixer", "shell:jekyllBuild", "watch"]);
     grunt.registerTask("deploy", ["buildcontrol:pages"]);
+    grunt.registerTask('htmlmini', ['htmlmin']);
 };
 // ping url's for search engines
-//http://submissions.ask.com/ping?sitemap=http://example.com/sitemap.xml
-//http://www.bing.com/webmaster/ping.aspx?siteMap=http://example.com/sitemap.xml
-//http://www.google.com/webmasters/sitemaps/ping?sitemap=http://example.com/sitemap.xml
+//http://submissions.ask.com/ping?sitemap=http://maniana.strefa.pl/sitemap.xml
+//http://www.bing.com/webmaster/ping.aspx?siteMap=http://maniana.strefa.pl/sitemap.xml
+//http://www.google.com/webmasters/sitemaps/ping?sitemap=http://maniana.strefa.pl/sitemap.xml
